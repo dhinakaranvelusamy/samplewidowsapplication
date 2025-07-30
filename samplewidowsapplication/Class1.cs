5fc2b62bfde91b24685d6444eb1e4d2a8b6eebf9
Vignesh.E <eswaranevignesh@gmail.com>
2025-07-30 13:38:58 +0530

    Arithmetic operation done

--- samplewidowsapplication/Class1.cs
+++ samplewidowsapplication/Class1.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Linq;
 namespace samplewidowsapplication
 {
     public class Class1

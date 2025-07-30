6f32532d684c6aea9c7614656beaa38a48da2cae
Vignesh.E <eswaranevignesh@gmail.com>
2025-07-30 13:49:51 +0530

    done programs on c#

---
+++ samplewidowsapplication/Hii.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace samplewidowsapplication
+{
+    class Hii
+    {
+        public void Welcome()
+        {
+            Console.WriteLine("Welcome to learn c#");
+        }
+    }
+}

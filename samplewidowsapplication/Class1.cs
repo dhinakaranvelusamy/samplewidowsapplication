d79472864f36af2fdbb5d95fbbed6cc333dd53ca
Vignesh.E <eswaranevignesh@gmail.com>
2025-07-30 13:12:22 +0530

    Revert ".."
    This reverts commit 1ca9c363f510f8e3d6d59959cd4ec770542e21c9.

--- samplewidowsapplication/Class1.cs
+++ samplewidowsapplication/Class1.cs
@@ -8,23 +8,15 @@ namespace samplewidowsapplication
 {
     public class Class1
     {
-        public void show() 
+        public void show()
         {
             int a = 8;
             int b = 7;
             int c = 8;
             int d = 6;
             Console.WriteLine(b - a);
-<<<<<<< HEAD
             Console.WriteLine(c * d);
 
-=======
-            Console.WriteLine(a + b)(modual) ;
-            int d =65 ;
-            int f = 56;
-            Console.WriteLine( d + f )(d - f );
-            
->
         }
     }
 }

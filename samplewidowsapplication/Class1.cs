a5ae48f957458d96900cbfc892429a4baf247ff2
Vignesh.E <eswaranevignesh@gmail.com>
2025-07-30 11:54:06 +0530

    Add project files.

---
+++ samplewidowsapplication/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace samplewidowsapplication
+{
+    public class Class1
+    {
+        public void show()
+        {
+            int a = 98;
+            int b = 76;
+            Console.WriteLine(b - a);
+
+        }
+    }
+}
---
+++ samplewidowsapplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace samplewidowsapplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 66 + 34 * 8  /4; int b = 33 + 17 /5 *4 ;
+            int c = 56 - 06 -76; int d = 77 + 23 *34 ;
+            Console.WriteLine(d / b);
+            Console.WriteLine(c * a);
+        }
+    }
+}
